bb545a0bfe62ed19986838b2c1c710b7e31f2d0f
Dmitry Ternovoy <zuganianin@gmail.com>
2023-06-01 15:04:29 +0700

    income configs

---
+++ Assets/Scripts/CoreLogic/Busines/Configs/Scripts/BusinessConfig.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace CoreLogic.Business.Configs {
+    
+    [CreateAssetMenu(fileName = "Business Config", menuName = "Business/Business Config")]
+    public class BusinessConfig : ScriptableObject
+    {
+        public float incomeCoolDown = 3.0f;
+        public int basePrice = 3;
+        public int baseIncome = 3;
+    }
+}
